fd0d91856e3835fdf9256def3456540708da67d7
devnick10 <nickbhoyar683@gmail.com>
2026-06-27 00:19:37 +0530

    refactor: extract prisma into packages/db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 export type ExtendedPrismaClient = ReturnType<typeof createPrismaClient>;
