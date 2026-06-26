1b0369f5da74986b25a845d957c98a17aa400d0d
devnick10 <nickbhoyar683@gmail.com>
2026-06-26 22:59:16 +0530

    chore: remove old server

--- server/src/controllers/healthcheck.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express"
-
-const healthCheck = (_req:Request, res:Response) => {
-    res.status(200).json({ message: "server is healthy" })
-}
-
-export {
-    healthCheck
-}
--- server/src/routes/healthcheck.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "../controllers/healthcheck.controller";
-const healthCheckRouter = Router();
-
-
-healthCheckRouter.get('/',healthCheck)
-
-export { healthCheckRouter };
-
